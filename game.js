621fed5e0afe358b1c1acc824fa68a750b110982
Jakub Rebeka <jakubrebeka@gmail.com>
2022-03-16 16:14:36 +0100

    Feature: Timer is working

--- game.js
+++ game.js
@@ -19,9 +19,10 @@ function initGame() {
   	modal.style.display = "block";
 
   	const button = document.querySelector("#button")
-
 	count_score();
+
 	}
+
   	button.addEventListener("click", function () {
   		let top = getRandomInt(98);
   		let left = getRandomInt(98);
@@ -31,12 +32,42 @@ function initGame() {
   	});
 }
 
+ function countdown(duration,display) {
+ 	let timer = duration, minutes, seconds;
+ 	setInterval(function () {
+ 		minutes = parseInt(timer / 60, 10);
+ 		seconds = parseInt(timer % 60, 10);
+
+ 		minutes = minutes < 10 ? "0" + minutes : minutes;
+ 		seconds = seconds < 10 ? "0" + seconds : seconds;
+
+ 		display.innerText = minutes + ":" + seconds;
+
+
+
+ 		if (--timer < 0) {
+ 			timer = duration;
+ 		}
+ 	}, 1000);
+}
+window.onload = function (){
+	let time = document.getElementById("myBtn")
+ time.addEventListener('click',(duration) =>{
+	let oneMinute = 60,
+			 display = document.querySelector('#time');
+	countdown(oneMinute,display)
+
+
+
+ })}
+
 function count_score(){
-	var score_num = document.getElementById("score-num")
-	var score = 0;
- 	var pop = document.getElementById("button");
+	let score_num = document.getElementById("score-num")
+	let score = 0;
+ 	let pop = document.getElementById("button");
 	pop.addEventListener("click", () =>{
 		score += 1;
 		console.log(score);
 		score_num.innerText="Score: "+ score;
 	})}
+
