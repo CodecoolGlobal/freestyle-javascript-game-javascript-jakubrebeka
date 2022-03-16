a3ceb62d29ee552d358d5572b84175da9bc8a738
mBartal88 <miklos.bartal@gmail.com>
2022-03-16 10:25:12 +0100

    Init game basics, single pop up button

--- game.js
+++ game.js
@@ -2,6 +2,15 @@ initGame();
 
 function initGame() {
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
+    	// Get the modal
+	let modal = document.getElementById("myModal");
+
+	// Get the button that opens the modal
+	let btn = document.getElementById("myBtn");
+
+	// When the user clicks the button, open the modal 
+	btn.onclick = function() {
+  	modal.style.display = "block";
+}
 
 }
