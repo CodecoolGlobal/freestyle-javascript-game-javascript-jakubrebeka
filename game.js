dfbf948acbb2c4541a2ba521a406e7ffa8321a3a
imi <szabimi12@gmail.com>
2022-03-16 11:18:01 +0100

    added score counter

--- game.js
+++ game.js
@@ -11,6 +11,15 @@ function initGame() {
 	// When the user clicks the button, open the modal 
 	btn.onclick = function() {
   	modal.style.display = "block";
+	}
+	score();
 }
-
-}
+function score(){
+	var score_num = document.getElementById("score-num")
+	var score = 0;
+ 	var pop = document.getElementById("button");
+	pop.addEventListener("click", () =>{
+		score += 1;
+		console.log(score);
+		score_num.innerText="Score: "+ score;
+	})}
